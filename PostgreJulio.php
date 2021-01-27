902e06814049165c4a6115efdad903c4db800680
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-01-27 20:00:28 -0300

    Update PostgreJulio.php

--- PostgreJulio.php
+++ PostgreJulio.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Postgres Júlio</title>
 </head>
-<body>
-    <h1>Teste Postgres Júlio</h1>
-    <?php
-        echo "Julio César R. Piau";
-    ?>
-</body>
+    <body>
+        <h1>Teste Postgres Júlio</h1>
+        <?php
+            echo "Julio César R. Piau";
+            echo "27/01/2021";
+            echo "123";
+        ?>
+    </body>
 </html>
 
